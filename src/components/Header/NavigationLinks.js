de116e665fb92f54b9af817479b76d2a26cf07ac
oenzocabral <cabraes2@gmail.com>
2023-10-02 21:49:46 -0300

    update about me image

--- src/components/Header/NavigationLinks.js
+++ src/components/Header/NavigationLinks.js
@@ -5,7 +5,7 @@ function NavigationLinks() {
   return (
     <ul className="nav nav-pills d-flex justify-content-center">
       <li className="nav-item"><Link to={"/projects"} className="nav-link">Projects</Link></li>
-      <li className="nav-item"><Link to={"/"} className="nav-link">Home</Link></li>
+      <li className="nav-item"><Link to={"/Portfolio"} className="nav-link">Home</Link></li>
       <li className="nav-item"><Link to={"/about"} className="nav-link">About Me</Link></li>
       <li className="nav-item dropdown">
         <p class="nav-link dropdown-toggle" id="navbarDropdownContact" role="button" data-bs-toggle="dropdown" aria-expanded="false">
